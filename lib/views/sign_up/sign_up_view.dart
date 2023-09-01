3e4deaf7cda9fe4d59303bdab0e4d9eab267ab30
hritik602 <hritikrajkarn@gmail.com>
2023-09-01 17:55:30 +0545

    feat: add sign up view and view model

--- lib/views/sign_up/sign_up_view.dart
+++ lib/views/sign_up/sign_up_view.dart
@@ -2,62 +2,77 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:stacked/stacked.dart';
+import 'package:thuprai_stacked_app/views/sign_up/model/sign_up_request.dart';
 import 'package:thuprai_stacked_app/widgets/dumb_widgets/input_text_field/input_text_field_widget.dart';
 import 'package:thuprai_stacked_app/widgets/dumb_widgets/primary/primary_button_widget.dart';
 import 'sign_up_view_model.dart';
 
-class SignUpView extends StatelessWidget {
+class SignUpView extends StatefulWidget {
+  const SignUpView({super.key});
+
+  @override
+  State<SignUpView> createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
   final TextEditingController _fullNameController = TextEditingController();
 
-  SignUpView({super.key});
+  final TextEditingController _emailController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<SignUpViewModel>.reactive(
+    return ViewModelBuilder<SignUpViewModel>.nonReactive(
       viewModelBuilder: () => SignUpViewModel(),
       builder: (BuildContext context, SignUpViewModel viewModel, Widget? _) {
         return Scaffold(
-            appBar: AppBar(),
-            body: SafeArea(
-                minimum: EdgeInsets.all(0.02.sw),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("Full Name"),
-                    SizedBox(
-                      height: 0.01.sh,
-                    ),
-                    InputTextFieldWidget(
-                      controller: _fullNameController,
-                    ),
-                    const Text("Email "),
-                    SizedBox(
-                      height: 0.01.sh,
-                    ),
-                    InputTextFieldWidget(
-                      controller: _fullNameController,
-                    ),
-                    SizedBox(
-                      height: 0.01.sh,
-                    ),
-                    const Text("Password"),
-                    SizedBox(
-                      height: 0.01.sh,
-                    ),
-                    InputTextFieldWidget(
-                      controller: _fullNameController,
-                    ),
-                    SizedBox(
-                      height: 0.01.sh,
-                    ),
-                    PrimaryButtonWidget(
-                      buttonTitle: "Sign Up",
-                      onTap: () {
-                        debugPrint("Clicked");
-                      },
-                    )
-                  ],
-                )));
+          appBar: AppBar(),
+          body: SafeArea(
+              minimum: EdgeInsets.all(0.02.sw),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Full Name"),
+                  SizedBox(
+                    height: 0.01.sh,
+                  ),
+                  InputTextFieldWidget(
+                    controller: _fullNameController,
+                  ),
+                  const Text("Email "),
+                  SizedBox(
+                    height: 0.01.sh,
+                  ),
+                  InputTextFieldWidget(
+                    controller: _emailController,
+                  ),
+                  SizedBox(
+                    height: 0.01.sh,
+                  ),
+                  const Text("Password"),
+                  SizedBox(
+                    height: 0.01.sh,
+                  ),
+                  InputTextFieldWidget(
+                    controller: _passwordController,
+                  ),
+                  SizedBox(
+                    height: 0.01.sh,
+                  ),
+                  PrimaryButtonWidget(
+                    buttonTitle: "Sign Up",
+                    onTap: () {
+                      viewModel.signUpUser(SignUpRequest(
+                          name: _fullNameController.text,
+                          email: _emailController.text,
+                          password: _passwordController.text));
+                    },
+                  )
+                ],
+              )),
+        );
       },
     );
   }
--- lib/views/sign_up/sign_up_view_model.dart
+++ lib/views/sign_up/sign_up_view_model.dart
@@ -1,3 +1,28 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:thuprai_stacked_app/core/locator.dart';
+import 'package:thuprai_stacked_app/core/router_constants.dart';
+import 'package:thuprai_stacked_app/views/sign_up/model/sign_in_response.dart';
+import 'package:thuprai_stacked_app/views/sign_up/model/sign_up_request.dart';
+import 'package:thuprai_stacked_app/views/sign_up/service/auth_service.dart';
 
-class SignUpViewModel extends BaseViewModel {}
+class SignUpViewModel extends BaseViewModel {
+  Future<void> signUpUser(SignUpRequest request) async {
+    if (request.email?.isEmpty == true &&
+        request.name?.isEmpty == true &&
+        request.password?.isEmpty == true) {
+      locator<DialogService>().showDialog(title: "Please fill all the details");
+    } else {
+      SignInResponse _signResponse =
+          await locator<AuthService>().signUpUser(request);
+      if (_signResponse.apiError != null) {
+        locator<DialogService>().showDialog(
+          title: "Error",
+        );
+      } else {
+        //Save user token in cache and navigate to home page
+        locator<NavigationService>().replaceWith(homeViewRoute);
+      }
+    }
+  }
+}
