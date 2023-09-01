e21a6cc26adf2ee08c97ef6eca6301dc66bbf82c
hritik602 <hritikrajkarn@gmail.com>
2023-09-01 17:53:55 +0545

    feat: add home view , home view model , model and service class

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:thuprai_stacked_app/views/home/model/book_model.dart';
+import 'package:thuprai_stacked_app/widgets/dumb_widgets/book_list_tile/book_list_tile_widget.dart';
 import 'home_view_model.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<HomeViewModel>.reactive(
+      viewModelBuilder: () => HomeViewModel(),
       builder: (BuildContext context, HomeViewModel viewModel, Widget? _) {
         return Scaffold(
-          appBar: AppBar(),
-          body: const Center(
-            child: Text('Home View'),
-          ),
-        );
+            appBar: AppBar(),
+            body: SafeArea(
+                minimum: const EdgeInsets.all(10),
+                child: FutureBuilder(
+                  future: viewModel.fetchBooks(1),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    BookReleaseModel bookReleaseModel =
+                        snapshot.data as BookReleaseModel;
+
+                    return ListView.builder(
+                        itemCount: bookReleaseModel.results?.length,
+                        itemBuilder: (context, index) {
+                          return BookListTile(
+                            books: bookReleaseModel.results![index],
+                          );
+                        });
+                  },
+                )));
       },
-      viewModelBuilder: () => HomeViewModel(),
     );
   }
 }
--- lib/views/home/home_view_model.dart
+++ lib/views/home/home_view_model.dart
@@ -1,3 +1,10 @@
 import 'package:stacked/stacked.dart';
+import 'package:thuprai_stacked_app/core/locator.dart';
+import 'package:thuprai_stacked_app/views/home/model/book_model.dart';
+import 'package:thuprai_stacked_app/views/home/service/home_service.dart';
 
-class HomeViewModel extends BaseViewModel {}
+class HomeViewModel extends BaseViewModel {
+  Future<BookReleaseModel> fetchBooks(int page) async {
+    return await locator<HomeService>().fetchAllNewReleaseBooks(page);
+  }
+}
