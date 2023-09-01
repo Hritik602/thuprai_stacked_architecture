2c5a1bd03e4382a21e8df7ad045e91c0727c03c8
hritik602 <hritikrajkarn@gmail.com>
2023-09-01 17:52:24 +0545

    feta: register auth and homer service as a lazy signleton

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:thuprai_stacked_app/views/home/service/home_service.dart';
+import 'package:thuprai_stacked_app/views/sign_up/service/auth_service.dart';
 
 final GetIt locator = GetIt.instance;
 
@@ -10,5 +12,8 @@ class LocatorInjector {
     locator.registerLazySingleton(() => DialogService());
 
     locator.registerLazySingleton(() => SnackbarService());
+
+    locator.registerLazySingleton(() => AuthService());
+    locator.registerLazySingleton(() => HomeService());
   }
 }
