d78326fb751f20b671f5ca9a306ce94507c5a8f7
hritik602 <hritikrajkarn@gmail.com>
2023-09-01 17:57:16 +0545

    feat: add splash view and view model.

--- lib/views/splash/splash_view.dart
+++ lib/views/splash/splash_view.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 
 import 'package:stacked/stacked.dart';
-import 'package:thuprai_stacked_app/utils/app_image.dart';
 import 'splash_view_model.dart';
 
 class SplashView extends StatelessWidget {
@@ -12,25 +9,26 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<SplashViewModel>.nonReactive(
+      viewModelBuilder: () => SplashViewModel(),
       builder:
           (BuildContext context, SplashViewModel viewModel, Widget? child) {
+        ///For [Test] navigating to another route.
+        viewModel.navigate();
         return Scaffold(
-          appBar: AppBar(
-            title: Text("Splash"),
-          ),
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                  //  color: Colors.amber,
-                  width: 1.sw,
-                  height: 0.5.sh,
-                  child: SvgPicture.asset(AppImage.thupraiLogo)),
+              Center(
+                child: Text(
+                  "Thuprai",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              )
             ],
           ),
         );
       },
-      viewModelBuilder: () => SplashViewModel(),
     );
   }
 }
--- lib/views/splash/splash_view_model.dart
+++ lib/views/splash/splash_view_model.dart
@@ -1,8 +1,15 @@
-import 'dart:math';
+import 'dart:async';
 
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:thuprai_stacked_app/core/locator.dart';
+import 'package:thuprai_stacked_app/core/router_constants.dart';
 
 class SplashViewModel extends BaseViewModel {
-  ///
-  ///
+  //Navigate to sign up screen
+  void navigate() {
+    Timer(const Duration(seconds: 2), () {
+      locator<NavigationService>().replaceWith(signUpViewRoute);
+    });
+  }
 }
