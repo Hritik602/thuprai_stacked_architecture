6b16b693d77d9eadfd8c7efeecfdd3b2dfe40f36
hritik602 <hritikrajkarn@gmail.com>
2023-09-05 16:31:43 +0545

    feat: add on tap method to view book detail page.

--- lib/widgets/dumb_widgets/book_list_tile/book_list_tile_widget.dart
+++ lib/widgets/dumb_widgets/book_list_tile/book_list_tile_widget.dart
@@ -1,52 +1,57 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:thuprai_stacked_app/views/home/model/book_model.dart';
 
-class BookListTile extends StatefulWidget {
-  const BookListTile({super.key, this.books});
+class BookListTile extends StatelessWidget {
+  const BookListTile({
+    Key? key,
+    this.books,
+    this.onTap,
+  }) : super(key: key);
   final Books? books;
-  @override
-  State<BookListTile> createState() => _BookListTileState();
-}
-
-class _BookListTileState extends State<BookListTile> {
+  final Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 5,
-      child: Container(
-        height: 0.23.sh,
-        padding: EdgeInsets.all(5.sp),
-        child: Row(
-          children: [
-            Container(
-              height: 0.2.sh,
-              width: 0.3.sw,
-              padding: EdgeInsets.all(5.sp),
-              child: CachedNetworkImage(
-                placeholder: (context, url) =>
-                    const Center(child: CircularProgressIndicator()),
-                imageUrl: widget.books?.frontCover ??
-                    'https://via.placeholder.com/200x150',
+    return GestureDetector(
+      onTap: onTap,
+      child: Material(
+        elevation: 5,
+        child: Container(
+          height: 0.23.sh,
+          padding: EdgeInsets.all(5.sp),
+          child: Row(
+            children: [
+              Container(
+                height: 0.2.sh,
+                width: 0.3.sw,
+                padding: EdgeInsets.all(5.sp),
+                child: CachedNetworkImage(
+                  placeholder: (context, url) =>
+                      const Center(child: CircularProgressIndicator()),
+                  imageUrl: books?.frontCover ??
+                      'https://via.placeholder.com/200x150',
+                ),
               ),
-            ),
-            SizedBox(
-              width: 0.01.sw,
-            ),
-            Container(
-              height: 0.2.sh,
-              width: 0.6.sw,
-              padding: EdgeInsets.all(5.sp),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(widget.books?.title ?? "Book Name"),
-                ],
+              SizedBox(
+                width: 0.01.sw,
               ),
-            )
-          ],
+              Container(
+                height: 0.2.sh,
+                width: 0.6.sw,
+                padding: EdgeInsets.all(5.sp),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(books?.title ?? "Book Name"),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
