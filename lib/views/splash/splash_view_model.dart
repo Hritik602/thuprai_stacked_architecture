98596104caf88afff04fae66c1ac526a062d43c5
hritik602 <hritikrajkarn@gmail.com>
2023-09-05 16:31:15 +0545

    feat: check user authentication and  navigate from splash view .

--- lib/views/splash/splash_view_model.dart
+++ lib/views/splash/splash_view_model.dart
@@ -4,12 +4,20 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:thuprai_stacked_app/core/locator.dart';
 import 'package:thuprai_stacked_app/core/router_constants.dart';
+import 'package:thuprai_stacked_app/utils/cache_service.dart';
 
 class SplashViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
   //Navigate to sign up screen
   void navigate() {
-    Timer(const Duration(seconds: 2), () {
-      locator<NavigationService>().replaceWith(signUpViewRoute);
+    Timer(const Duration(seconds: 2), () async {
+      bool isAuth = await CacheService.isUserLogin();
+
+      if (isAuth) {
+        _navigationService.replaceWith(homeViewRoute);
+      } else {
+        _navigationService.replaceWith(loginViewRoute);
+      }
     });
   }
 }
