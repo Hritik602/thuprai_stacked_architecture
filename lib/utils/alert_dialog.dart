0d93aa6e7cb212d9792bd5358f0f6b1623db432f
hritik602 <hritikrajkarn@gmail.com>
2023-09-06 10:13:24 +0545

    feat: add custom alert dialog

---
+++ lib/utils/alert_dialog.dart
@@ -0,0 +1,9 @@
+// import 'package:stacked_services/stacked_services.dart';
+// import 'package:thuprai_stacked_app/core/locator.dart';
+
+// class AlertDialogService {
+//   final _dialogService = locator<DialogService>();
+//    void errorAlertDialog() {
+//     _dialogService.registerCustomDialogBuilders(builders)
+//   }
+// }
